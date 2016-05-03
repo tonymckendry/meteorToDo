dcb47fae6eb7b1f0bd5e32a72d94aecef7341de1
TonyMcKendry <TonyMcKendry@gmail.com>
2016-05-03 08:32:50 -0600

    mostly done with Meteor Tutorial

---
+++ client/main.js
@@ -0,0 +1,14 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import todosList from '../imports/components/todosList/todosList';
+import '../imports/startup/accounts-config.js';
+
+angular.module('simple-todos', [
+  angularMeteor,
+  todosList.name,
+  'accounts.ui'
+]);
+
+function onReady(){
+
+}
---
+++ imports/components/todosList/todosList.js
@@ -0,0 +1,63 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import { Meteor } from 'meteor/meteor'
+import { Tasks } from '../../api/tasks.js'
+
+import template from './todosList.html'
+
+class TodosListCtrl {
+  constructor($scope){
+    $scope.viewModel(this);
+
+    this.hideCompleted = false;
+
+    this.helpers({
+      tasks(){
+        const selector = {};
+
+        if (this.getReactively('hideCompleted')){
+          selector.checked = {
+            $ne: true
+          };
+        }
+        return Tasks.find(selector, {
+          sort: {
+            createdAt: -1
+          }
+        })
+      },
+      incompleteCount(){
+        return Tasks.find({
+          checked:{
+            $ne: true
+          }
+        }).count();
+      },
+      currentUser(){
+        return Meteor.user();
+      }
+    })
+  }
+
+    addTask(newTask) {
+      Meteor.call('tasks.insert', newTask);
+
+      this.newTask = '';
+    }
+
+    setChecked(task){
+      Meteor.call('tasks.setChecked', task._id, !task.checked);
+    }
+
+    removeTask(task){
+      Meteor.call('tasks.remove', task._id);
+    }
+  }
+
+export default angular.module('todosList', [
+  angularMeteor
+])
+  .component('todosList', {
+    templateUrl: 'imports/components/todosList/todosList.html',
+    controller: ['$scope', TodosListCtrl]
+  });
